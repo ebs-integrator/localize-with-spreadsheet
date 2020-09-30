3e6400d81bb7dbb3707cb30dad70e76f7b5272de
Pavel Sorokin <pavel@amplerbikes.com>
2020-09-30 21:29:40 +0300

    Clean up, also fix a bug where sheetsFilter was ignored by GSReader

--- index.js
+++ index.js
@@ -1,76 +1,70 @@
-var GSReader = require('./core/LineReader.js').GS;
-var FileWriter = require('./core/Writer.js').File;
-var Transformer = require('./core/Transformer.js');
+const GSReader = require('./core/LineReader.js').GS;
+const FileWriter = require('./core/Writer.js').File;
+const Transformer = require('./core/Transformer.js');
 
-var Gs2File = function (reader, writer) {
-    this._reader = reader;
-    this._writer = writer;
-};
-
-Gs2File.fromGoogleSpreadsheet = function (spreadsheetKey, sheets) {
-    var gs2file = new Gs2File(new GSReader(spreadsheetKey, sheets),
-        new FileWriter());
-
-    return gs2file;
-};
-
-Gs2File.prototype.setValueCol = function (valueCol) {
-    this._defaultValueCol = valueCol;
+const Gs2File = function(reader, writer) {
+  this._reader = reader
+  this._writer = writer
 }
 
-Gs2File.prototype.setKeyCol = function (keyCol) {
-    this._defaultKeyCol = keyCol;
+Gs2File.fromGoogleSpreadsheet = function(spreadsheetKey, sheets) {
+  return new Gs2File(
+    new GSReader(spreadsheetKey, sheets),
+    new FileWriter()
+  )
 }
 
-Gs2File.prototype.setFormat = function (format) {
-    this._defaultFormat = format;
+Gs2File.prototype.setValueCol = function(valueCol) {
+  this._defaultValueCol = valueCol
 }
 
-Gs2File.prototype.setEncoding = function (encoding) {
-    this._defaultEncoding = encoding;
+Gs2File.prototype.setKeyCol = function(keyCol) {
+  this._defaultKeyCol = keyCol
 }
 
-Gs2File.prototype.save = function (outputPath, opts, cb) {
-    console.log('saving ' + outputPath);
-    var self = this;
+Gs2File.prototype.setFormat = function(format) {
+  this._defaultFormat = format
+}
 
-    opts = opts || {};
+Gs2File.prototype.setEncoding = function(encoding) {
+  this._defaultEncoding = encoding
+}
 
-    var keyCol = opts.keyCol,
-        valueCol = opts.valueCol,
-        format = opts.format,
-        encoding = opts.encoding;
+Gs2File.prototype.save = async function(outputPath, opts) {
+  console.log('saving ' + outputPath)
+  const self = this
 
-    if (!keyCol) {
-        keyCol = this._defaultKeyCol;
+  opts = opts || {}
+
+  let keyCol = opts.keyCol
+  let valueCol = opts.valueCol
+  let format = opts.format
+  let encoding = opts.encoding
+
+  if (!keyCol) {
+    keyCol = this._defaultKeyCol
+  }
+
+  if (!valueCol) {
+    valueCol = this._defaultValueCol
+  }
+
+  if (!format) {
+    format = this._defaultFormat
+  }
+
+  if (!encoding) {
+    encoding = this._defaultEncoding
+    if (!encoding) {
+      encoding = 'utf8'
     }
+  }
 
-    if (!valueCol) {
-        valueCol = this._defaultValueCol;
-    }
+  const lines = await this._reader.select(keyCol, valueCol)
+  if (lines) {
+    const transformer = Transformer[format || 'android']
+    self._writer.write(outputPath, encoding, lines, transformer, opts)
+  }
+}
 
-    if (!format) {
-        format = this._defaultFormat;
-    }
-
-    if(!encoding) {
-        encoding = this._defaultEncoding;
-        if(!encoding) {
-            encoding = 'utf8';
-        }
-    }
-
-    this._reader.select(keyCol, valueCol).then(function (lines) {
-        if (lines) {
-            var transformer = Transformer[format || 'android'];
-            self._writer.write(outputPath, encoding, lines, transformer, opts);
-        }
-
-        if (typeof(cb) == 'function') {
-            cb();
-        }
-    });
-
-};
-
-module.exports = Gs2File;
+module.exports = Gs2File
